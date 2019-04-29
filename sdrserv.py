a076634167b0a13edd185c5b54821e88b86fa8e4
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2019-04-29 10:43:07 +0200

    created class streamserver

--- sdrserv.py
+++ sdrserv.py
@@ -15,9 +15,8 @@ samplerate=2400000
 gain=20
 outputfile='-'
 
-HOST = ''
-PORT = 33000
-BUFSIZ = 1024
+host = ''
+port = 33000
 
 
 parser = argparse.ArgumentParser(description='Get iq datastream form radio device')
@@ -50,75 +49,84 @@ if args.gain:
     gain=args.gain
 
 if args.port:
-    PORT=int(args.port)
-
-ADDR = (HOST, PORT)
-print(ADDR)
-
+    port=int(args.port)
 
 # TCP Server
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address, file=sys.stderr)
-        #client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+class StreamServer(Thread):
+    def __init__(self, server):
+        Thread.__init__(self)
+        self.clients={}
+        self.addresses={}
+        self.server=server
+        self.bufsiz = 1024
+
+    def run(self):
+        ACCEPT_THREAD = Thread(target=self.accept_incoming_connections)
+        ACCEPT_THREAD.start()
+    
+    def accept_incoming_connections(self):
+        """Sets up handling for incoming clients."""
+        while True:
+            client, client_address = self.server.accept()
+            print("%s:%s has connected." % client_address, file=sys.stderr)
+            #client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+            self.addresses[client] = client_address
+            Thread(target=self.handle_client, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
-    name = "test"
-    #name = client.recv(BUFSIZ).decode("utf8")
-    #welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    #client.send(bytes(welcome, "utf8"))
-    #msg = "%s has joined the chat!" % name
-    #broadcast(bytes(msg, "utf8"))
-    clients[client] = name
+    def handle_client(self,client):  # Takes client socket as argument.
+        """Handles a single client connection."""
+        name = "test"
+        #name = client.recv(BUFSIZ).decode("utf8")
+        #welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+        #client.send(bytes(welcome, "utf8"))
+        #msg = "%s has joined the chat!" % name
+        #broadcast(bytes(msg, "utf8"))
+        self.clients[client] = name
 
-    while True:
-        try:
-            msg = client.recv(BUFSIZ)
-            if msg == bytes("{quit}", "utf8"):
-                client.send(bytes("{quit}", "utf8"))
-                client.close()
-                del clients[client]
+        while True:
+            try:
+                msg = client.recv(self.bufsiz)
+                if msg == bytes("{quit}", "utf8"):
+                    client.send(bytes("{quit}", "utf8"))
+                    client.close()
+                    del self.clients[client]
+                    break
+            except:
+                del self.clients[client]
                 break
-        except:
-            del clients[client]
-            break
-        
-
-
-def broadcast(msg, prefix=""):
-    """Broadcasts a message to all the clients."""
-    for sock in list(clients): # list is a copy of clients
-        try:
-            sock.send(msg)
-        except:
-            del clients[sock]
-            print("Closed connection by peer!", file=sys.stderr)
             
-clients = {}
-addresses = {}
 
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+    def broadcast(self,msg, prefix=""):
+        """Broadcasts a message to all the clients."""
+        for sock in list(self.clients): # list is a copy of clients
+            try:
+                sock.send(msg)
+            except:
+                del self.clients[sock]
+                print("Closed connection by peer!", file=sys.stderr)
+            
 
-SERVER.listen(5)
-print("Waiting for tcp connection...", sys.stderr)
-ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-ACCEPT_THREAD.start()
+
 # END TCP Server
+
+server = socket(AF_INET, SOCK_STREAM)
+server.bind((host, port))
+server.listen(5)
+
+iqstream=StreamServer(server)
+iqstream.start()
+print("Waiting for tcp connection...", sys.stderr)
+
 
 def handler(signum, frame):
     print('Strg+c', signum)
     os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-    ACCEPT_THREAD.join()
-    SERVER.close()
+    iqstream.stop()
+    iqstream.join()
+    server.close()
     sys.exit()
 
 signal.signal(signal.SIGINT, handler)
@@ -135,7 +143,7 @@ while True:
     if out=='' and p.poll() != None:
         break
     if out != '':
-        broadcast(out)        
+        iqstream.broadcast(out)        
         #sys.stdout.buffer.write(out)
         #sys.stdout.flush()
 
