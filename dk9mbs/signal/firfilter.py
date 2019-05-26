43922e8893c9bb46b46eafc659e6c8b064a79bf7
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2019-05-26 22:44:18 +0200

    sin generator added

--- dk9mbs/signal/firfilter.py
+++ dk9mbs/signal/firfilter.py
@@ -1,93 +1,105 @@
-from numpy import cos, sin, pi, absolute, arange
-from scipy.signal import kaiserord, lfilter, firwin, freqz
-from pylab import figure, clf, plot, xlabel, ylabel, xlim, ylim, title, grid, axes, show
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+import sys
+
+def plot_response(fs, taps, title):
+    w, h = signal.freqz(taps, [1], worN=2000)
+    plt.figure()
+    plt.plot(0.5*fs*w/np.pi, 20*np.log10(np.abs(h)))
+    plt.ylim(-40, 5)
+    plt.xlim(0, 0.5*fs)
+    plt.grid(True)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Gain (dB)')
+    plt.title(title)
+    plt.show()
+
 
 class FirFilter:
-    def __init__(self):
+    """  Fir filter
+    fs = 44100.0       # Sample rate, Hz
+    cutoff = 5000.0    # Desired cutoff frequency, Hz
+    trans_width = 250  # Width of transition from pass band to stop band, Hz
+    numtaps = 125      # Size of the FIR filter.
+    """
+    def __init__(self, fs, cutoff, trans_width, numtaps):
         self.__window=[]
-        self.__window_width=5
         self.__average=0
         self.__taps=[]
-        self.__init(100,400)
 
-        for _ in range(0,self.__window_width):
+        self.__fs=fs
+        self.__cutoff=cutoff
+        self.__trans_width=trans_width
+        self.__num_taps=numtaps
+
+        self.__init_taps(fs, cutoff, trans_width, numtaps)
+
+        for _ in range(0,self.__num_taps):
             self.__window.append(0)
-            #self.__window[x]=0
 
     def get_window(self):
+        """ Get the current fir filter window as array """
         return self.__window
 
     def get_taps(self):
         return self.__taps
 
-    def add_value(self, value):
-        average=0
-        for x in range(self.__window_width-1,0,-1):
-            self.__window[x]=self.__window[x-1]
-            average+=self.__window[x]
-        
-        self.__window[0]=value 
-        average+=value
-        self.__average=average/self.__window_width 
-
     def get_filter_value(self):
         return self.__average
 
+    def add_value(self, value):
+        average=0
+        for x in range(self.__num_taps-1,0,-1):
+            self.__window[x]=self.__window[x-1]
+            average+=self.__window[x]*self.__taps[x]
+        
+        self.__window[0]=value 
+        average+=value*self.__taps[0]
 
-    def __init(self,sample_rate):
-        #------------------------------------------------
-        # Create a signal for demonstration.
-        #------------------------------------------------
-
-        #sample_rate = 100.0
-        #nsamples = 400
-        #t = arange(nsamples) / sample_rate
-        #x = cos(2*pi*0.5*t) + 0.2*sin(2*pi*2.5*t+0.1) + \
-        #        0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + \
-        #            0.1*sin(2*pi*23.45*t+.8)
+        self.__average=average/self.__num_taps 
+        print(self.__average)
 
 
-        #------------------------------------------------
-        # Create a FIR filter and apply it to x.
-        #------------------------------------------------
+    def __init_taps(self,fs, cutoff, trans_width, numtaps):
 
-        # The Nyquist rate of the signal.
-        nyq_rate = sample_rate / 2.0
-
-        # The desired width of the transition from pass to stop,
-        # relative to the Nyquist rate.  We'll design the filter
-        # with a 5 Hz transition width.
-        width = 5.0/nyq_rate
-
-        # The desired attenuation in the stop band, in dB.
-        ripple_db = 60.0
-
-        # Compute the order and Kaiser parameter for the FIR filter.
-        N, beta = kaiserord(ripple_db, width)
-
-        # The cutoff frequency of the filter.
-        cutoff_hz = 10.0
-
-        # Use firwin with a Kaiser window to create a lowpass FIR filter.
-        taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-
-        self.__taps=taps
-        # Use lfilter to filter x with the FIR filter.
-        #filtered_x = lfilter(taps, 1.0, x)
+        self.__taps = signal.remez(numtaps, [0, cutoff, cutoff + trans_width, 0.5*fs],[1, 0], Hz=fs)
 
 
 
 
 
 if __name__ == "__main__":
-    filter=FirFilter()
-    print(filter.get_window())
+    sample_rate = 100.0
+    nsamples = 100
+    f=20.0
+ 
+    t =  np.arange(nsamples, step=0.2) / sample_rate
+    samples = np.sin(2*np.pi*f*t) 
 
-    for x in range(1,6):
+    plt.xlabel('Angle [rad]')
+    plt.ylabel('Amplitude')
+
+    fft=np.fft.fft(samples)
+    n=int(len(fft)/2+1)
+
+    filter=FirFilter(sample_rate,5.0,1.0,10)
+
+    out=[]
+    for x in samples:
         filter.add_value(x)
-        print('x=>%s value=>%s filter=>%s' % (x,filter.get_filter_value(), filter.get_window()))
-        
+        out.append(filter.get_filter_value())
 
-    print(filter.get_window())
-    #print(filter.get_filter_value())
 
+    fft=np.fft.fft(out)
+    n=int(len(fft)/2+1)
+    #plt.plot( np.abs(fft[:n]  ))
+    plt.plot(out)
+    plt.show()
+       
+    #print(filter.get_window() )
+
+    #plt.figure()
+    #plt.grid(True)
+    #plt.plot(filter.get_taps(),'bo-', linewidth=2)
+    #plt.show()
